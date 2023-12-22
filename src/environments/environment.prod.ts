04cd54dbb658a70aa58da47da92d7376a621eb51
vatsal <vso.opash@gmail.com>
2023-12-22 17:37:37 +0530

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
-  production: true,
+  production: false,
   hmr: false,
-  serverUrl: 'https://api.healing.tube/api/v1/',
+  serverUrl: 'https://api.reefer.social/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.healing.tube/',
+  socketUrl: 'https://api.reefer.social',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
-
 };
